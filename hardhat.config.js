82cd00fee6cdfa03bc20b1b5fa11a8084b24eb14
Lautaro N Gonzalez <produgraf2000@gmail.com>
2022-09-18 12:10:52 -0300

    agregada configuracion

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/' + projectId,
+      accounts: [
+        privateKey
+      ]
+    }
+  }
+};
